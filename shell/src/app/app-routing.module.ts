e4eed271616c6186e687e6d4b0736d8d842a0574
jonathan earl <earl.jonathan@gmail.com>
2021-07-06 20:10:38 -0700

    add shell

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'banner',
+    loadChildren: () => import('banner/Module').then(m => m.BannerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ shell/src/decl.d.ts
@@ -0,0 +1,2 @@
+// declare module 'mfe1/Module';
+declare module 'banner/Module';
