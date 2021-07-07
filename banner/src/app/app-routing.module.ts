561b8e7d62c96aea07829b19f3e9b8bc165edc58
jonathan earl <earl.jonathan@gmail.com>
2021-07-06 20:10:31 -0700

    add remote banner

---
+++ banner/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'banner',
+    loadChildren: () => import('./banner/banner.module').then(m => m.BannerModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
